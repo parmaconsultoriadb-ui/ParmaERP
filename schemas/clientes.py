e7905d1d8705b71bd146ced5fee2f369edb03e2c
ParmaConsultoria <parmaconsultoriadb@gmail.com>
2025-10-30 14:48:48 -0300

    Update clientes.py

--- schemas/clientes.py
+++ schemas/clientes.py
@@ -3,11 +3,11 @@ from typing import Optional
 
 class Cliente(BaseModel):
     id: Optional[int] = None
-    data: Optional[str] = Field(None, description="Data de cadastro no formato DD/MM/YYYY")
+    data: Optional[str] = Field(None, description="Data de cadastro (DD/MM/YYYY)")
     cliente: Optional[str] = Field(None, description="Nome ou razão social do cliente")
     nome: Optional[str] = Field(None, description="Nome do contato principal")
     cidade: Optional[str] = None
     uf: Optional[str] = None
     telefone: Optional[str] = None
     email: Optional[EmailStr] = None
-    created_at: Optional[str] = Field(None, description="Data/hora de criação DD/MM/YYYY HH:MM:SS")
+    created_at: Optional[str] = Field(None, description="Data/hora de criação (DD/MM/YYYY HH:MM:SS)")
