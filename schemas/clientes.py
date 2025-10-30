c17642800abc9cafa707425780bdfd7cb32cc594
ParmaConsultoria <parmaconsultoriadb@gmail.com>
2025-10-30 11:23:19 -0300

    Update clientes.py

--- schemas/clientes.py
+++ schemas/clientes.py
@@ -2,7 +2,9 @@ from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
 
 class Cliente(BaseModel):
-    id: str
+    id: int | None = None
     nome: str = Field(min_length=2, max_length=120)
     email: Optional[EmailStr] = None
-    cnpj: Optional[str] = Field(default=None, min_length=14, max_length=14)
+    cidade: Optional[str] = None
+    uf: Optional[str] = Field(default=None, min_length=2, max_length=2)
+    telefone: Optional[str] = None
